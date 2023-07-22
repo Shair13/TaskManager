fc92ddf2b78ca04acaad018b0520a5a61aed3b48
Shair13 <shad350@gmail.com>
2023-07-22 17:37:21 +0200

    displayOptions class

--- src/main/java/pl/coderslab/taskmanager/TaskManager.java
+++ src/main/java/pl/coderslab/taskmanager/TaskManager.java
@@ -1,4 +1,26 @@
 package pl.coderslab.taskmanager;
 
+import java.util.Arrays;
+
 public class TaskManager {
+
+    public static void main(String[] args) {
+        displayOptions();
+    }
+
+    public class ConsoleColors {
+        public static final String BLUE = "\033[0;34m";
+        public static final String RESET = "\033[0m";
+    }
+
+    public static void displayOptions() {
+
+        System.out.println(ConsoleColors.BLUE + "Please select an option:" + ConsoleColors.RESET);
+        String[] managerOptions = {"add", "remove", "list", "exit"};
+        for(int i = 0; i < managerOptions.length; i++){
+            System.out.println(managerOptions[i]);
+        }
+    }
+
+
 }
